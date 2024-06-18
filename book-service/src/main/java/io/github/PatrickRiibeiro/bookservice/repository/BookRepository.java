ac97adf0c01430cff172ad37babe744beb97712a
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-06-18 20:21:36 -0300

    reset

--- book-service/src/main/java/io/github/PatrickRiibeiro/bookservice/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.PatrickRiibeiro.bookservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.github.PatrickRiibeiro.bookservice.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
