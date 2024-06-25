c19cd7f14aa6a89ea14e989fcb463e1caaace72c
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-06-25 18:12:31 -0300

    refactor app math

--- rest-with-spring-boot/src/main/java/br/com/patrickriibeiro/rest/controllers/MathController.java
+++ rest-with-spring-boot/src/main/java/br/com/patrickriibeiro/rest/controllers/MathController.java
@@ -5,180 +5,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.patrickriibeiro.exceptions.UnsuportedMathOperationException;
-import lombok.extern.slf4j.Slf4j;
+import br.com.patrickriibeiro.services.MathService;
 
 @RestController
 @RequestMapping("/v1/restservice")
-@Slf4j
 public class MathController {
+	
+	private MathService mathService;
+	
+	public MathController(MathService mathService) {
+		this.mathService = mathService;
+	}
 	
 	@GetMapping("/sum/{firstNumber}/{secondNumber}")
 	public Double sum(@PathVariable(name = "firstNumber") String firstNumber, @PathVariable(name = "secondNumber") String secondNumber) {
-		
-		Double firstNumberConvert = this.convertValue(firstNumber);
-		Double secondNumberConvert = this.convertValue(secondNumber);
-		
-		
-		if(!this.isNumeric(firstNumber) || !this.isNumeric(secondNumber)) {
-			throw new UnsuportedMathOperationException("Please set a numeric value.");
-		}
-		
-		if (firstNumberConvert < Double.MIN_VALUE || firstNumberConvert > Double.MAX_VALUE) {
-            throw new UnsuportedMathOperationException("FirstNumber eis out of range permited.");
-        }
-        
-        if (secondNumberConvert < Double.MIN_VALUE || secondNumberConvert > Double.MAX_VALUE) {
-            throw new UnsuportedMathOperationException("SecondNumber is out of range permited.");
-        }
-        
-        // Validação de infinito
-        if (Double.isInfinite(firstNumberConvert) || Double.isInfinite(secondNumberConvert)) {
-            throw new UnsuportedMathOperationException("One of this values is infinity.");
-        }
-
-        // Aviso para zero
-        if (firstNumberConvert == 0.0 || secondNumberConvert == 0.0) {
-             log.info("WARNING: one of this value is zero");   
-        } 
-        
-		return Math.round((firstNumberConvert + secondNumberConvert) * 100.0) / 100.0;
+		return mathService.sum(firstNumber, secondNumber);
 	}
 	
 	@GetMapping("/sub/{firstNumber}/{secondNumber}")
 	public Double sub(@PathVariable(name = "firstNumber") String firstNumber, @PathVariable(name = "secondNumber") String secondNumber) {
-		
-		Double firstNumberConvert = this.convertValue(firstNumber);
-		Double secondNumberConvert = this.convertValue(secondNumber);
-		
-		if(!this.isNumeric(firstNumber) || !this.isNumeric(secondNumber)) {
-			throw new UnsuportedMathOperationException("Please set a numeric value.");
-		}
-		
-		    if (firstNumberConvert < Double.MIN_VALUE || firstNumberConvert > Double.MAX_VALUE) {
-	            throw new UnsuportedMathOperationException("FirstNumber eis out of range permited.");
-	        }
-	        
-	        if (secondNumberConvert < Double.MIN_VALUE || secondNumberConvert > Double.MAX_VALUE) {
-	            throw new UnsuportedMathOperationException("SecondNumber is out of range permited.");
-	        }
-
-	        // Validação de infinito
-	        if (Double.isInfinite(firstNumberConvert) || Double.isInfinite(secondNumberConvert)) {
-	            throw new UnsuportedMathOperationException("One of this values is infinity.");
-	        }
-
-	        // Aviso para zero
-	        if (firstNumberConvert == 0.0 || secondNumberConvert == 0.0) {
-	             log.info("WARNING: one of this value is zero");   
-	        } 
-		
-		return Math.round((firstNumberConvert - secondNumberConvert) * 100.0) / 100.0;
+		return mathService.sub(firstNumber, secondNumber);	
 	}
 	
 	@GetMapping("/porc/{firstNumber}/{secondNumber}")
 	public Double porc(@PathVariable(name = "firstNumber") String firstNumber, @PathVariable(name = "secondNumber") String secondNumber) {
-		
-		Double firstNumberConvert = this.convertValue(firstNumber);
-		Double secondNumberConvert = this.convertValue(secondNumber);
-		
-		if(!this.isNumeric(firstNumber) || !this.isNumeric(secondNumber)) {
-			throw new UnsuportedMathOperationException("Please set a numeric value.");
-		}
-		
-		    if (firstNumberConvert < Double.MIN_VALUE || firstNumberConvert > Double.MAX_VALUE) {
-	            throw new UnsuportedMathOperationException("FirstNumber eis out of range permited.");
-	        }
-	        
-	        if (secondNumberConvert < Double.MIN_VALUE || secondNumberConvert > Double.MAX_VALUE) {
-	            throw new UnsuportedMathOperationException("SecondNumber is out of range permited.");
-	        }
-
-	        // Validação de infinito
-	        if (Double.isInfinite(firstNumberConvert) || Double.isInfinite(secondNumberConvert)) {
-	            throw new UnsuportedMathOperationException("One of this values is infinity.");
-	        }
-
-	        // Aviso para zero
-	        if (firstNumberConvert == 0.0 || secondNumberConvert == 0.0) {
-	             log.info("WARNING: one of this value is zero");   
-	        } 
-		
-		return (firstNumberConvert * secondNumberConvert) / 100;
+		return mathService.porc(firstNumber, secondNumber);		
 	}
 	
 	@GetMapping("/mult/{firstNumber}/{secondNumber}")
 	public Double mult(@PathVariable(name = "firstNumber") String firstNumber, @PathVariable(name = "secondNumber") String secondNumber) {
-		
-		Double firstNumberConvert = this.convertValue(firstNumber);
-		Double secondNumberConvert = this.convertValue(secondNumber);
-		
-		if(!this.isNumeric(firstNumber) || !this.isNumeric(secondNumber)) {
-			throw new UnsuportedMathOperationException("Please set a numeric value.");
-		}
-		
-		    if (firstNumberConvert < Double.MIN_VALUE || firstNumberConvert > Double.MAX_VALUE) {
-	            throw new UnsuportedMathOperationException("FirstNumber eis out of range permited.");
-	        }
-	        
-	        if (secondNumberConvert < Double.MIN_VALUE || secondNumberConvert > Double.MAX_VALUE) {
-	            throw new UnsuportedMathOperationException("SecondNumber is out of range permited.");
-	        }
-
-	        // Validação de infinito
-	        if (Double.isInfinite(firstNumberConvert) || Double.isInfinite(secondNumberConvert)) {
-	            throw new UnsuportedMathOperationException("One of this values is infinity.");
-	        }
-
-	        // Aviso para zero
-	        if (firstNumberConvert == 0.0 || secondNumberConvert == 0.0) {
-	             log.info("WARNING: one of this value is zero");   
-	        } 
-		
-		return Math.round((firstNumberConvert * secondNumberConvert) * 100.0) / 100.0;
+		return mathService.mult(firstNumber, secondNumber);		
 	}
 	
 	
 	@GetMapping("/div/{firstNumber}/{secondNumber}")
 	public Double div(@PathVariable(name = "firstNumber") String firstNumber, @PathVariable(name = "secondNumber") String secondNumber) {
-		
-		Double firstNumberConvert = this.convertValue(firstNumber);
-		Double secondNumberConvert = this.convertValue(secondNumber);
-		
-		if(!this.isNumeric(firstNumber) || !this.isNumeric(secondNumber)) {
-			throw new UnsuportedMathOperationException("Please set a numeric value.");
-		}
-		
-		    if (firstNumberConvert < Double.MIN_VALUE || firstNumberConvert > Double.MAX_VALUE) {
-	            throw new UnsuportedMathOperationException("FirstNumber eis out of range permited.");
-	        }
-	        
-	        if (secondNumberConvert < Double.MIN_VALUE || secondNumberConvert > Double.MAX_VALUE) {
-	            throw new UnsuportedMathOperationException("SecondNumber is out of range permited.");
-	        }
-
-	        // Validação de infinito
-	        if (Double.isInfinite(firstNumberConvert) || Double.isInfinite(secondNumberConvert)) {
-	            throw new UnsuportedMathOperationException("One of this values is infinity.");
-	        }
-
-	        // Aviso para zero
-	        if (firstNumberConvert == 0.0 || secondNumberConvert == 0.0) {
-	             log.info("WARNING: one of this value is zero");   
-	        } 
-		
-		return Math.round((firstNumberConvert / secondNumberConvert) * 100.0) / 100.0;
+		return mathService.div(firstNumber, secondNumber);	
 	}
 	
-	
-	
-	private boolean isNumeric(String number) {
-		if(number == null || number.isEmpty()) return false;
-		return number.matches("-?\\d+(\\.\\d+)?");
-	}
-	
-	private Double convertValue(String number) {
-		return Double.parseDouble(number.replaceAll(",","."));
-	}
-
 }
---
+++ rest-with-spring-boot/src/main/java/br/com/patrickriibeiro/services/MathService.java
@@ -0,0 +1,14 @@
+package br.com.patrickriibeiro.services;
+
+public interface MathService {
+
+	public Double sum(String firstNumber, String secondNumber);
+	
+	public Double sub(String firstNumber, String secondNumber);
+	
+	public Double mult(String firstNumber, String secondNumber);
+	
+	public Double div(String firstNumber, String secondNumber);
+	
+	public Double porc(String firstNumber, String secondNumber);
+}
