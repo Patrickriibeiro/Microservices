0576b577e1d91e0ad66f9b089c980b7c23360b5d
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-06-25 17:48:04 -0300

    firstCommit

---
+++ rest-with-spring-boot/src/main/java/br/com/patrickriibeiro/Startaup.java
@@ -0,0 +1,13 @@
+package br.com.patrickriibeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startaup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startaup.class, args);
+	}
+
+}
---
+++ rest-with-spring-boot/src/main/java/br/com/patrickriibeiro/exceptions/UnsuportedMathOperationException.java
@@ -0,0 +1,18 @@
+package br.com.patrickriibeiro.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsuportedMathOperationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UnsuportedMathOperationException(String ex) {
+		super(ex);
+	}
+	
+}
