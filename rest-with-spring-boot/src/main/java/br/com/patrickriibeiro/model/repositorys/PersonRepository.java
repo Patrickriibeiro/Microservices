5de4935fb624cb851ce4568c8938df5b9ff3d6a1
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-07-10 13:44:51 -0300

    ajustes services

--- rest-with-spring-boot/src/main/java/br/com/patrickriibeiro/model/repositorys/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.patrickriibeiro.model.repositorys;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.patrickriibeiro.model.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Long>{
-
-}
