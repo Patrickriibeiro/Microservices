ce656fe8b6705258edbce6b138081c7bf8411337
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-07-10 12:49:19 -0300

    Configurando a camada proxy utilizando o feign

--- book-service/src/main/java/br/com/patrickriibeiro/controller/BookController.java
+++ book-service/src/main/java/br/com/patrickriibeiro/controller/BookController.java
@@ -1,18 +1,15 @@
 package br.com.patrickriibeiro.controller;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.patrickriibeiro.model.Book;
+import br.com.patrickriibeiro.proxy.CambioProxy;
 import br.com.patrickriibeiro.repository.BookRepository;
-import br.com.patrickriibeiro.response.Cambio;
 
 @RestController
 @RequestMapping(value = "/book-service")
@@ -24,23 +21,25 @@ public class BookController {
 	@Autowired
 	private BookRepository bookRepository;
 
+	@Autowired
+	private CambioProxy proxy;
+
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(@PathVariable Long id, @PathVariable String currency) {
-
 		var book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
-
-		HashMap<String, String> params = new HashMap<>();
-		params.put("amount", book.getPrice().toString());
-		params.put("from", "USD");
-		params.put("to", currency);
-
-		var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
-				Cambio.class, params);
-
 		book.setEnvironment(environment.getProperty("local.server.port"));
-		book.setPrice(response.getBody().getConvertedValue());
+		book.setPrice(proxy.getCambio(book.getPrice(), "USD", currency).getConvertedValue());
 		return book;
-
 	}
+
+	/*
+	 * HashMap<String, String> params = new HashMap<>(); params.put("amount",
+	 * book.getPrice().toString()); params.put("from", "USD"); params.put("to",
+	 * currency);
+	 * 
+	 * var response = new RestTemplate().getForEntity(
+	 * "http://localhost:8000/cambio-service/{amount}/{from}/{to}", Cambio.class,
+	 * params);
+	 */
 
 }
---
+++ book-service/src/main/java/br/com/patrickriibeiro/proxy/CambioProxy.java
@@ -0,0 +1,20 @@
+package br.com.patrickriibeiro.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.com.patrickriibeiro.response.Cambio;
+
+@FeignClient(name = "cambio-service",
+url = "http://localhost:8000")
+public interface CambioProxy {
+	
+	@GetMapping(value = "/cambio-service/{amount}/{from}/{to}", 
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Cambio getCambio(@PathVariable("amount") Double amount, @PathVariable("from") String from,
+			@PathVariable("to") String to);
+
+}
