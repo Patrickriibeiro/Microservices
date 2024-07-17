c8d16d8bd1fda8a97374036c61ab4d430be99e14
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-07-17 17:34:21 -0300

    Configurando rotas manualmente e logers filter

--- api-gateway/src/main/java/br/com/patrickriibeiro/configuration/ApiGateWayConfiguration.java
+++ api-gateway/src/main/java/br/com/patrickriibeiro/configuration/ApiGateWayConfiguration.java
@@ -1,30 +1,21 @@
 package br.com.patrickriibeiro.configuration;
 
-import java.util.function.Function;
-
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGateWayConfiguration {
-	
+
 	@Bean
-	RouteLocator gateWayRouter(RouteLocatorBuilder builder ) {
-		Function<PredicateSpec, Buildable<Route>> function = p -> {
-				return p.path("/get")
-						.filters( f -> f.addRequestHeader("hello", "World"))
-						.uri("http://httpbin.org:80");
-		};
-				
+	RouteLocator gateWayRouter(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(function)
-				.build();
+				.route(p -> p.path("/get")
+						.filters(f -> f.addRequestHeader("hello", "World").addRequestParameter("Hello", "Word"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/cambio-service/**").uri("lb://cambio-service"))
+				.route(p -> p.path("/book-service/**").uri("lb://book-service")).build();
 	}
-	
 
 }
