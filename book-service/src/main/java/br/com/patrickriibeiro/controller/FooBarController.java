be530288f215419952398db68e71bd459625437d
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-07-18 20:49:51 -0300

    Wait Duration and exponencial Backoff

--- book-service/src/main/java/br/com/patrickriibeiro/controller/FooBarController.java
+++ book-service/src/main/java/br/com/patrickriibeiro/controller/FooBarController.java
@@ -1,5 +1,7 @@
 package br.com.patrickriibeiro.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,19 @@ import io.github.resilience4j.retry.annotation.Retry;
 @RequestMapping(value = "/book-service")
 public class FooBarController {
 
+	public static final Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
 	@GetMapping("/foo-bar")
-	@Retry(name = "foo-bar")
+	@Retry(name = "foo-bar", fallbackMethod = "metodoTratativo")
 	public String fooBar() {
+		logger.info("Testando retry resilience.");
 		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
 		return response.getBody();
+	}
+
+	public String metodoTratativo(Exception e) {
+		return "Testando metodo Tratativo ." + e.getCause();
+
 	}
 
 }
