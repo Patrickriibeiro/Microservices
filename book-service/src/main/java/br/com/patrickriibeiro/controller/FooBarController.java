cdc49be360c49e182aba3e16a44559b638057960
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-07-18 21:24:20 -0300

    RateLimiter Bulkhead

--- book-service/src/main/java/br/com/patrickriibeiro/controller/FooBarController.java
+++ book-service/src/main/java/br/com/patrickriibeiro/controller/FooBarController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
+//import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping(value = "/book-service")
@@ -16,7 +18,9 @@ public class FooBarController {
 	public static final Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
 	@GetMapping("/foo-bar")
-	@Retry(name = "foo-bar", fallbackMethod = "metodoTratativo")
+	//@Retry(name = "foo-bar", fallbackMethod = "metodoTratativo")
+	//@CircuitBreaker(name = "default", fallbackMethod = "metodoTratativo")
+	@RateLimiter(name = "default")
 	public String fooBar() {
 		logger.info("Testando retry resilience.");
 		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
