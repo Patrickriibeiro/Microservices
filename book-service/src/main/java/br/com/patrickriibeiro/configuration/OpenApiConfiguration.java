be9434bca74908015e84ddea9c05a7ad80d89c57
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2024-07-20 14:11:57 -0300

    ajustes pom

--- book-service/src/main/java/br/com/patrickriibeiro/configuration/OpenApiConfiguration.java
+++ book-service/src/main/java/br/com/patrickriibeiro/configuration/OpenApiConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.patrickriibeiro.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
